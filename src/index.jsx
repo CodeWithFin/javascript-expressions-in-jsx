a8679797800b754f0f2a977f2f0a9e19e129208d
Finley <finley.mwachia12@gmail.com>
2024-08-08 12:56:00 +0300

    add files for the project

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
